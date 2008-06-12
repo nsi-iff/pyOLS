becba45016ed3d0ee756656b6c0f79ae89124ff0
wolever <david@wolever.net>
2008-06-12 21:30:11 +0000

    First version of the model

--- pronto/model.py
+++ pronto/model.py
@@ -3,7 +3,15 @@
 from elixir import *
 from sqlalchemy import UniqueConstraint
 
+class Namespace(Entity):
+    has_field('id', Integer, primary_key=True)
+    has_field('name', Unicode, unique=True)
+
+    using_options(tablename='namespaces')
+
+
 class Relation(Entity):
+    has_field('id', Integer, primary_key=True)
     belongs_to('namespace', of_kind='Namespace')
     has_field('name', String)
     has_field('revelance', Float, default=1)
@@ -18,13 +26,30 @@ class Relation(Entity):
     using_options(tablename='relations')
     using_table_options(UniqueConstraint('namespace_id', 'name'))
 
+
 class Keyword(Entity):
+    has_field('id', Integer, primary_key=True)
+    belongs_to('namespace', of_kind='Namespace')
     has_field('name', Unicode)
     has_field('association', Unicode)
-    has_many('relations', of_kind='Relation')
+
+    has_many('left_relations', of_kind='KeywordRelationship', inverse='left')
+    has_many('right_relations', of_kind='KeywordRelationship', inverse='right')
 
     using_options(tablename='keywords')
     using_table_options(UniqueConstraint('namespace_id', 'name', 'association'))
 
-class Namespace(Entity):
-    has_field('name', Unicode)
+
+class KeywordRelationship(Entity):
+    _kws={'primary_key': True, 'ondelete': 'cascade'}
+    belongs_to('left', of_kind='Keyword', **_kws)
+    belongs_to('relation', of_kind='Relation', **_kws)
+    belongs_to('right', of_kind='Keyword', **_kws)
+    del _kws
+
+    using_options(tablename='keyword_relationships')
+    
+
+metadata.bind = "sqlite:///:memory:"
+metadata.bind.echo = True
+setup_all(True)
